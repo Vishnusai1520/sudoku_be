94ac5372129de551f672254c20f0000df6955ecd
mgupta-tby <mgupta@terryberry.com>
2024-08-15 12:18:21 +0530

    main: Added auth key to APIs and added profile API to router

--- app/router/sudoku.py
+++ app/router/sudoku.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
+from fastapi import Depends
 
 from handler.sudoku import * 
+from authy import *
 
 route = APIRouter()
 
 # @route.post('/login')
 
-@route.get('/sudoku')
+@route.get('/sudoku', dependencies=[Depends(verify_api_key)])
 async def get_sudoku(difficulty:str):
     return await generate_puzzle(difficulty)
 
-@route.post('/solve')
+@route.post('/solve', dependencies=[Depends(verify_api_key)])
 async def solve_sudoku(puzzle:dict):
     return await sudoku_solution(puzzle['grid'])

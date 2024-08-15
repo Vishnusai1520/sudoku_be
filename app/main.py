a0873b1d11d8ff554cedc5bba673682fd93558cc
bmkgupta <manikantagupta17@gmail.com>
2024-08-15 13:28:54 +0530

    auth2: Added auth changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from router import sudoku
+from router import sudoku, profile
 
 app = FastAPI(title='sudoku',description = '',version = '1.0.0')
 
@@ -21,3 +21,4 @@ async def root():
     return {"message": "Sudoku"}
 
 app.include_router(sudoku.route,prefix = '/api/v1')
+app.include_router(profile.route,prefix = '/api/v1')
